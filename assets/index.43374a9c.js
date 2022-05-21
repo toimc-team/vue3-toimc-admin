94aaaa5e6c21638f72b0005b12cf0f84a48b84ad
toimc <51934415+toimc@users.noreply.github.com>
2022-05-21 06:45:24 +0000

    Deploying to gh-pages from @ toimc-team/vue3-toimc-admin@1721f51dcf00d0976dcb39e3640549dbc153e03b 🚀

---
+++ assets/index.43374a9c.js
@@ -0,0 +1,1 @@
+import{_ as o,e as t,r,o as n,c as s,b as c}from"./index.f395e6a2.js";const a=t({setup(){return{}}}),_={class:"p-4"};function p(i,d,u,f,m,l){const e=r("router-view");return n(),s("div",_,[c(e)])}var x=o(a,[["render",p]]);export{x as default};
---
+++ assets/menu1-2.64b4140c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index.f395e6a2.js";const r={};function o(t,a){return c(),n("div",null,"menu1-2")}var s=e(r,[["render",o]]);export{s as default};
